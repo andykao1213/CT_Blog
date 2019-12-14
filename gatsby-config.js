71f0459ff595f75ad4c991a564220521054be870
高銓澤 <chuan@gaoquanzede-MacBook-Pro.local>
2019-12-14 20:12:23 +0800

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `C.T. Blog`,
+    author: `Andrew Kao`,
+    description: `Personal blog of a software engineer.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
